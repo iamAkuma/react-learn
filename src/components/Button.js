58f7dcfbd42753032711b13e5223e6a38d9c46ca
Saral Bajracharya <rmanbajra@gmail.com>
2024-01-03 17:03:41 +0545

    react learning

---
+++ src/components/Button.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Button = () => {
+
+    const user = {
+        name: 'Hedy Lamarr',
+        imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+        imageSize: 90,
+      };
+
+  return (
+    <>
+    <div>
+    <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </div>
+    </>
+  )
+}
+
+export default Button
